636583b52bfb40063560850a1a33c17c84b14431
RestiSri28 <restisriastuti28@gmail.com>
2026-09-03 06:05:05 +0800

    Day02.java

---
+++ Day02.java
@@ -0,0 +1,24 @@
+public class Day02 {
+    public static void main(String[] args) {
+
+        // Menampilkan judul menggunakan println()
+        System.out.println(" BIODATA DIRI");
+
+        // println() digunakan untuk menampilkan teks
+        // dan langsung pindah ke baris berikutnya
+        System.out.println("Nama       : Resti SriAstuti");
+        System.out.println("Jurusan    : Teknik Informatika");
+        System.out.println("Universitas: Universitas Sulawesi Barat");
+
+        // print digunakan untuk menampilkan teks
+        // tanpa langsung pindah ke baris berikutnya
+        System.out.print("Status     : ");
+        System.out.print("Mahasiswa");
+
+        // printf() digunakan untuk menampilkan teks dengan format tertentu
+        System.out.printf("\nUmur       : %d tahun", 18);
+
+        // Kembali menggunakan println()
+        System.out.println("\nHobi       : Membaca");
+    }
+}

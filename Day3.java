a239bc7723b618257cc25fe8b6193442c29446f1
RestiSri28 <restisriastuti28@gmail.com>
2026-09-04 12:36:20 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,24 @@
+public class Day03 {
+    public static void main(String[] args) {
+
+        // \n digunakan untuk membuat baris baru
+        System.out.println("Halo, nama saya Resti.\nSaya sedang belajar Java.");
+
+        // \t digunakan untuk membuat jarak seperti tab
+        System.out.println("Nama\t: Resti Sri Astuti");
+        System.out.println("Prodi\t: Teknik Informatika");
+
+        // \" digunakan untuk menampilkan tanda kutip
+        System.out.println("Saya berkata, \"Saya sedang belajar Java.\"");
+
+        // \\ digunakan untuk menampilkan tanda backslash
+        System.out.println("Contoh backslash: \\");
+
+        // \b digunakan untuk menghapus satu karakter sebelumnya
+        System.out.println("Halo\b!");
+
+        // \r digunakan untuk kembali ke awal baris
+        System.out.println("Java\rBelajar");
+
+    }
+}

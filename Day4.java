734ed7ee5645798e3aab53dade584c16fc36e26f
RestiSri28 <restisriastuti28@gmail.com>
2026-09-05 13:13:44 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,23 @@
+	public class Day04 {
+    public static void main(String[] args) {
+
+        // Mempelajari tipe data bilangan bulat
+        // byte digunakan untuk menyimpan angka yang kecil
+        // short digunakan untuk angka yang lebih besar dari byte
+        // int digunakan untuk bilangan bulat yang umum
+        // long digunakan untuk menyimpan angka yang sangat besar
+
+        byte usia = 18;
+        short tahunLahir = 2008;
+        int uangSaku = 50000;
+        long jumlahPenduduk = 2800000000L;
+
+        // Menampilkan data ke layar
+        System.out.println(" DATA NUMERIK");
+        System.out.println("Usia            : " + usia + " tahun");
+        System.out.println("Tahun Lahir     : " + tahunLahir);
+        System.out.println("Uang Saku       : Rp" + uangSaku);
+        System.out.println("Jumlah Penduduk : " + jumlahPenduduk);
+
+    }
+}

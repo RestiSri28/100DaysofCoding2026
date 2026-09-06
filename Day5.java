83e15fe5ed17729409306525c83712749143dbed
RestiSri28 <restisriastuti28@gmail.com>
2026-09-06 16:08:26 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,20 @@
+public class Day05 {
+    public static void main(String[] args) {
+
+        // Mempelajari tipe data pecahan
+        // double digunakan untuk menyimpan bilangan pecahan
+        // float digunakan untuk menyimpan bilangan pecahan
+        // dengan ukuran yang lebih kecil dibandingkan double
+
+        double tinggiBadan = 150.5;
+        float beratBadan = 50.5f;
+
+        // Menampilkan nilai ke layar
+        System.out.println("DATA BILANGAN PECAHAN");
+        System.out.println("Tinggi Badan : " + tinggiBadan + " cm");
+        System.out.println("Berat Badan  : " + beratBadan + " kg");
+
+        // Kesimpulan
+        System.out.println("\nSaya sedang belajar tipe data double dan float.");
+    }
+}

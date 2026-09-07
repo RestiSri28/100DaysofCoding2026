d11e6182220c7d55a643166d831f33504b74a4ce
RestiSri28 <restisriastuti28@gmail.com>
2026-09-07 18:14:17 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,20 @@
+public class Day06 {
+    public static void main(String[] args) {
+
+        // Mempelajari tipe data char dan boolean
+
+        // char digunakan untuk menyimpan satu karakter
+        char jenisKelamin = 'P';
+
+        // boolean digunakan untuk menyimpan nilai true atau false
+        boolean mahasiswaAktif = true;
+
+        // Menampilkan nilai ke layar
+        System.out.println("DATA KARAKTER DAN BOOLEAN");
+        System.out.println("Jenis Kelamin   : " + jenisKelamin);
+        System.out.println("Mahasiswa Aktif : " + mahasiswaAktif);
+
+        // Kesimpulan
+        System.out.println("\nSaya sedang belajar tipe data char dan boolean.");
+    }
+}

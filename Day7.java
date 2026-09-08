12102b15a0e8797eb90386835ab37324f509bd99
RestiSri28 <restisriastuti28@gmail.com>
2026-09-08 18:13:33 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+public class Day07 {
+    public static void main(String[] args) {
+
+        // Mempelajari tipe data String
+        // String digunakan untuk menyimpan teks
+
+        String nama = "Resti Sri Astuti";
+        String prodi = "Teknik Informatika";
+        String kampus = "Universitas Sulawesi Barat";
+
+        // Menampilkan data String ke layar
+        System.out.println(" DATA STRING");
+        System.out.println("Nama   : " + nama);
+        System.out.println("Prodi  : " + prodi);
+        System.out.println("Kampus : " + kampus);
+    }
+}

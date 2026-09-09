862b6ddf0d408b1e418379e45c49596de94713fe
RestiSri28 <restisriastuti28@gmail.com>
2026-09-09 18:39:25 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+public class Day08 {
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        int umur;
+
+        // Inisialisasi variabel
+        umur = 18;
+
+        System.out.println("Sebelum diubah : " + umur);
+
+        // Update variabel
+        umur = 19;
+
+        System.out.println("Sesudah diubah : " + umur);
+    }
+}

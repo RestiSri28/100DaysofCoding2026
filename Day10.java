82852eaef49aae7eaeebb44c53aaecad311a8dad
RestiSri28 <restisriastuti28@gmail.com>
2026-09-11 22:17:48 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,16 @@
+public class Day10 {
+    public static void main(String[] args) {
+
+        // Menampilkan batas minimal dan maksimal tipe data
+
+        System.out.println("=== BATAS TIPE DATA ===");
+
+        System.out.println("Byte  : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        System.out.println("Short : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+        System.out.println("Int   : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        System.out.println("Long  : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+
+        System.out.println("Float : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+        System.out.println("Double: " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+    }
+}

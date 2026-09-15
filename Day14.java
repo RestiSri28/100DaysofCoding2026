5640461a5b8fc8da93f191f2f3540ca28977805a
RestiSri28 <restisriastuti28@gmail.com>
2026-09-15 20:41:21 +0800

    Day14.java

--- Day14.java
+++ Day14.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
-
 public class PenjumlahanPengurangan {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        // Input
-        int angka1 = input.nextInt();
-        int angka2 = input.nextInt();
+        int angka1 = 20;
+        int angka2 = 8;
 
-        // Proses
         int penjumlahan = angka1 + angka2;
         int pengurangan = angka1 - angka2;
 
-        input.close();
-
-        // Output
         System.out.printf("Angka pertama      :\t%d%n", angka1);
         System.out.printf("Angka kedua        :\t%d%n", angka2);
         System.out.printf("Hasil penjumlahan  :\t%d%n", penjumlahan);

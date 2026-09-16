6ab25c09fc8634fec231af6766e81138ca577ad7
RestiSri28 <restisriastuti28@gmail.com>
2026-09-16 15:56:21 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class PerkalianPembagian {
+    public static void main(String[] args) {
+
+        int angka1 = 20;
+        int angka2 = 5;
+
+        int perkalian = angka1 * angka2;
+        int pembagian = angka1 / angka2;
+
+        System.out.printf("Angka pertama     :\t%d%n", angka1);
+        System.out.printf("Angka kedua       :\t%d%n", angka2);
+        System.out.printf("Hasil perkalian   :\t%d%n", perkalian);
+        System.out.printf("Hasil pembagian   :\t%d%n", pembagian);
+    }
+}

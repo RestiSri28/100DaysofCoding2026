24988a2bca3e2ec6a5e29252110c7a9ebb887cc9
RestiSri28 <restisriastuti28@gmail.com>
2026-09-21 19:45:48 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,11 @@
+public class PrimitifKeString {
+    public static void main(String[] args) {
+
+        double nilai = 85.5;
+
+        String teks = String.valueOf(nilai);
+
+        System.out.println("Nilai = " + nilai);
+        System.out.println("String = " + teks);
+    }
+}

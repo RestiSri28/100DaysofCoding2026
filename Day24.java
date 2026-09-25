8dc38a92737d826ae9afb1e81c05a724134258f6
RestiSri28 <restisriastuti28@gmail.com>
2026-09-25 18:11:16 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LuasPersegiPanjang {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang: ");
+        int panjang = input.nextInt();
+
+        System.out.print("Masukkan lebar: ");
+        int lebar = input.nextInt();
+
+        int luas = panjang * lebar;
+
+        System.out.println("\n HASIL PERHITUNGAN ");
+        System.out.println("Panjang : " + panjang);
+        System.out.println("Lebar   : " + lebar);
+        System.out.println("Luas    : " + luas);
+
+        input.close();
+    }
+}
